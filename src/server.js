53661fa9ab5e5b774a879d8afab81fd672b5e1c4
SukhrobCoder <101581646+Sukhrobjon9848@users.noreply.github.com>
2022-10-21 22:58:01 +0500

    Project's v1 is finished

--- src/server.js
+++ src/server.js
@@ -1,9 +1,25 @@
 import http from 'http'
 import HOST from './lib/network.js'
-
+import Express from './lib/routers.js'
 const PORT= process.cwd.PORT || 4000
 const Server =http.createServer((req,res)=>{
-    res.end("Hello")
+   let app= new Express(req,res)
+    console.log(req.url);
+   app.get('/',(req,res)=>{
+    app.engine('index')
+   })
+  
+   app.get('/public/css/styles.css',(req,res)=>{
+    app.engineCss('styles')
+   })
+
+   app.get('/images/logo.svg',(req,res)=>{
+    app.engineImg('logo')
+   })
+   app.get('/images/healthcare-illustration.png',(req,res)=>{
+    app.engineImgs('healthcare-illustration')
+   })
+
 })
 
 Server.listen(PORT,()=>{
